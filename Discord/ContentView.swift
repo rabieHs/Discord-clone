85b14fdba1a4e057f3993ae7a9ffc42d4e1cf546
rabieHs <rabiehoussaini@gmail.com>
2025-01-26 12:09:45 +0100

    Initial Commit

---
+++ Discord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Discord
+//
+//  Created by Apple on 26/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Discord/DiscordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordApp.swift
+//  Discord
+//
+//  Created by Apple on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiscordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
